ccf41cdd05854179b948c579edfb9defbd44429c
Saida Davis <saihdavis@gmail.com>
2020-08-16 23:17:06 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :base, :height, :hypotenuse, :equilateral, :isosceles, :scalene
+  
+  def initialize(base, height, hypotenuse)
+    @base = base
+    @height = height
+    @hypotenuse = hypotenuse
+  end 
+  
+  def kind
+    if !(@base + @height > @hypotenuse) || !(@height + @hypotenuse > @base) || !(@hypotenuse + @base > @height)
+        raise TriangleError
+    elsif (@base == @height) && (@height == @hypotenuse)
+        return :equilateral
+    elsif (@height == @hypotenuse) || (@base == @height) ||       (@hypotenuse == @base)
+        return :isosceles
+    else
+      return :scalene
+      end
+  end
+  
+    class TriangleError < StandardError
+  end
+
 end
